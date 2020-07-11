2992b01899092b0f286d5c7ad0f1a5ec6120d3c5
TuyenVu <tuyenvu14111999@gmail.com>
2020-07-11 13:09:16 +0700

    add

--- src/main/java/com/selfgrowth/dailyplanning/auth/security/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/selfgrowth/dailyplanning/auth/security/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -65,7 +65,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                 .claim("authorities", auth.getAuthorities().stream()
                         .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .setIssuedAt(new Date(now))
-                .setExpiration(new Date(now + jwtConfig.getExpiration() * 1000))  // in milliseconds
+                .setExpiration(new Date(now + jwtConfig.getExpiration() * 1000))
                 .signWith(SignatureAlgorithm.HS512, jwtConfig.getSecret().getBytes())
                 .compact();
 
